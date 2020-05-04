c51249f7750f026c169cb08b206dfa6937c0ca49
unknown <jeffrey.r.carter86@gmail.com>
2020-05-04 12:14:58 -0400

    added ManagerFactory class

---
+++ WinnerWinner/ManagerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinnerWinner
+{
+    class ManagerFactory
+    {
+        public ISweepstakesManager ChooseManagerType(string input)
+        {
+            switch (input.ToLower())
+            {
+                case "stack":
+                    return new SweepstakesStackManager();
+                case "queue":
+                    return new SweepstakesQueueManager();
+                default:
+                    return ChooseManagerType(input);
+            }
+        }
+    }
+}
